41c7228126280b613e2bf868584070b952deb732
Olimpia <olimpiacompean@prusuit.org>
2019-01-09 13:53:00 -0500

    Added Models

---
+++ Models/MagicModel.swift
@@ -0,0 +1,9 @@
+//
+//  MagicModel.swift
+//  TeamWork (Jason & Olimpia)
+//
+//  Created by Olimpia on 1/9/19.
+//  Copyright © 2019 Olimpia. All rights reserved.
+//
+
+import Foundation
---
+++ Models/PokemonModel.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonModel.swift
+//  TeamWork (Jason & Olimpia)
+//
+//  Created by Olimpia on 1/9/19.
+//  Copyright © 2019 Olimpia. All rights reserved.
+//
+
+import Foundation
